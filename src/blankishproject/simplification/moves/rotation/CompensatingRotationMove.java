ef09cef08a4f7f5515e37c64f3aaf33c9785722b
AsciiBunny <turbeau@asciibunny.com>
2022-02-01 03:56:38 +0100

    Fixed small CompensatingRotationMoves

--- src/blankishproject/simplification/moves/rotation/CompensatingRotationMove.java
+++ src/blankishproject/simplification/moves/rotation/CompensatingRotationMove.java
@@ -13,24 +13,31 @@ public class CompensatingRotationMove extends RotationMove {
 
     @Override
     protected Vector getRotationPoint(OrientationSet.Orientation orientation) {
-        var prevAngle = configuration.isStartConvex() ? 360 - configuration.getStartAngle() : configuration.getStartAngle();
-        var nextAngle = configuration.isEndConvex() ? 360 - configuration.getEndAngle() : configuration.getEndAngle();
+        var prevAngle = Vector.multiply(-1.0, configuration.previous.getDirection()).computeCounterClockwiseAngleTo(configuration.inner.getDirection());
+        prevAngle = prevAngle > Math.PI ? 2 * Math.PI - prevAngle : prevAngle;
+        var nextAngle = Vector.multiply(-1.0, configuration.inner.getDirection()).computeCounterClockwiseAngleTo(configuration.next.getDirection());
+        nextAngle = nextAngle > Math.PI ? 2 * Math.PI - nextAngle : nextAngle;
 
         var rotationAngle = orientation.getDirection().computeCounterClockwiseAngleTo(this.configuration.inner.getDirection());
-        if (rotationAngle > 90)
-            rotationAngle = Vector.multiply(-1.0, orientation.getDirection()).computeCounterClockwiseAngleTo(this.configuration.inner.getDirection());
+        if (rotationAngle > Math.PI)
+            rotationAngle = 2 * Math.PI - rotationAngle;
+        if (rotationAngle > 0.5 * Math.PI)
+            rotationAngle = Math.PI - rotationAngle;
 
-        var prevSegmentRatio = Math.sin(rotationAngle) / Math.sin(prevAngle);
-        var nextSegmentRatio = Math.sin(rotationAngle) / Math.sin(nextAngle);
+        var prevIntersectionAngle = Math.PI - rotationAngle - prevAngle;
+        var nextIntersectionAngle = Math.PI - rotationAngle - nextAngle;
 
         var k = Math.sin(rotationAngle);
+        var prevSegmentRatio = Math.sin(prevAngle);
+        var nextSegmentRatio = Math.sin(nextAngle);
+        var prevIntersectionRatio = Math.sin(prevIntersectionAngle);
+        var nextIntersectionRatio = Math.sin(nextIntersectionAngle);
 
-        var a = prevSegmentRatio * k - nextSegmentRatio * k;
-        var b = 2 * nextAngle * k;
-        var c = - nextAngle * k;
+        var a = (k * prevSegmentRatio) / prevIntersectionRatio - (k * nextSegmentRatio) / nextIntersectionRatio;
+        var b = (2 * k * nextSegmentRatio) / nextIntersectionRatio;
+        var c = (-nextSegmentRatio * k) / nextIntersectionRatio;
 
         var innerSegmentRatio = DoubleUtil.solveQuadraticEquationForSmallestPositive(a, b, c, DoubleUtil.EPS);
-
         var point = configuration.inner.getPointAlongPerimeter(innerSegmentRatio);
         return point;
     }
